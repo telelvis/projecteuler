1e3be2848d77d0087fff7d3e75ea75e8fa0ca52f
Vladimir Shingarev <telelvis@gmail.com>
2013-07-10 22:17:38 +0400

    problem 20

---
+++ projecteuler/problem20.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# coding: utf-8
+
+"""
+    n! means n × (n − 1) × ... × 3 × 2 × 1
+
+    For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+    and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 
+    = 27.
+
+    Find the sum of the digits in the number 100!
+"""
+
+print sum(map(int,str(reduce(lambda x,y: x*y, range(1, 101)))))
